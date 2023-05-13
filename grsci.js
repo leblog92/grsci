197b541b69cbf64e552212aab379d298c310abda
leblog92 <50671954+leblog92@users.noreply.github.com>
2023-05-13 18:56:31 +0200

    remove getElement
    & better readability

--- grsci.js
+++ grsci.js
@@ -1,16 +1,24 @@
-var div0=document.getElementById("div0");
-if(!div0){
-	jQuery('body').prepend('<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet"><style>.uni{position:fixed;z-index:10000;overflow-y:scroll;scrollbar-width:none;background-color:rgba(0,129,178,1);height:auto;padding:10px;margin:10px;border-radius:4px;color:#fff;font-family:"Roboto Mono",monospace;box-shadow: 0px 1px 6px 0px rgba(0, 0, 0, 0.12),0px 1px 4px 0px rgba(0, 0, 0, 0.12);transition:1s;}.uni::-webkit-scrollbar{display:none;}#div0{top:40px;width:270px;}#div1{top:40px;width:270px;}#div2{left:0;bottom:0;user-select:none;}#div3{left:70px;bottom:0;user-select:none;}</style><div title="Requête de Sélection" class="uni" id="div0" style="right:0;" contenteditable="false"><h3>👉GRSCI™ v4</h3>Générateur de Requête de Sélection sur <a style="color:white;text-decoration:none;" href="https://catalogue.mediatheque-rueilmalmaison.fr/" target="_blank"><u>Catalogue InMedia</u></a><hr style="border-top:0.1px solid white;"><div style="font-size:14px;">Pour créer votre séléction survolez les titres des documents pour générer la requête \'LocalNumber:####\' avec l\'opérateur \'OR\'. Ne fonctionne pas avec les ressources en ligne.<br><br></div></div><div title="Titres de la séléction" class="uni" id="div1" style="left:0;" contenteditable="false">Changelog GRSCI™ :<br><ul><li>Affichage des titres</li><li>Bouton Hide/Show</li><li>Bouton Reset</li></ul><img src="https://leblog92.github.io/grsci/cat.png" style="margin-bottom:-26px;" width="250px" height=auto"/></div><div title="Montre/cache l\'outil" class="uni" id="div2">Hide</div><div title="Efface la sélection" id="div3" class="uni">Reset</div>');
+if($("#div0").length==0){
+	jQuery('body').prepend('<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet"><style>.uni{position:fixed;z-index:10000;overflow-y:scroll;scrollbar-width:none;background-color:rgba(0,129,178,1);height:auto;padding:10px;margin:10px;border-radius:4px;color:#fff;font-family:"Roboto Mono",monospace;box-shadow: 0px 1px 6px 0px rgba(0, 0, 0, 0.12),0px 1px 4px 0px rgba(0, 0, 0, 0.12);transition:1s;}.uni::-webkit-scrollbar{display:none;}#div0{top:40px;width:270px;}#div1{top:40px;width:270px;}#div2{left:0;user-select:none;}#div3{left:70px;user-select:none;}</style><div title="Requête de Sélection" class="uni" id="div0" style="right:-300px;" contenteditable="false"><h3>👉GRSCI™ v4</h3>Générateur de Requête de Sélection sur <a style="color:white;text-decoration:none;" href="https://catalogue.mediatheque-rueilmalmaison.fr/" target="_blank"><u>Catalogue InMedia</u></a><hr style="border-top:0.1px solid white;"><div style="font-size:14px;">Pour créer votre séléction survolez les titres des documents pour générer la requête \'LocalNumber:####\' avec l\'opérateur \'OR\'. Ne fonctionne pas avec les ressources en ligne.<br><br></div></div><div title="Titres de la séléction" class="uni" id="div1" style="left:-300px;" contenteditable="false">Changelog GRSCI™ :<br><ul><li>Affichage des titres</li><li>Bouton Hide/Show</li><li>Bouton Reset</li></ul><img src="https://leblog92.github.io/grsci/cat.png" style="margin-bottom:-26px;" width="250px" height=auto"/></div><div title="Montre/cache l\'outil" class="uni" style="bottom:-50px;" id="div2">Hide</div><div title="Efface la sélection" id="div3"  style="bottom:-50px;" class="uni">Reset</div>');
 	$(document).ready(function(){
 		var origin=window.location.origin;
 		var arr0=[];
 		var arr1=[];
 		var shown=true;
-		$("#div1").on("change",function(){
-			alert("Handler for change called.");
-		});
+		$("#div0").attr("style","right:0px;");
+		$("#div1").attr("style","left:0px");
+		$("#div2").attr("style","bottom:0px");
+		$("#div3").attr("style","bottom:0px");
 		$("#div2").on("click",function(){
-			if(shown==true){$("#div0").attr("style","right:-300px;");$("#div1").attr("style","left:-300px");shown=false;$("#div2").html("Show");}else{$("#div0").attr("style","right:0;");$("#div1").attr("style","left:0;");shown=true;$("#div2").html("Hide");}
+			if(shown==true){
+				$("#div0").attr("style","right:-300px;");
+				$("#div1").attr("style","left:-300px");
+				shown=false;$("#div2").html("Show");
+			}else{
+				$("#div0").attr("style","right:0;");
+				$("#div1").attr("style","left:0;");
+				shown=true;$("#div2").html("Hide");
+			}
 		});
 		$("#div3").on("click",function(){
 			$(div0).html(arr0);
